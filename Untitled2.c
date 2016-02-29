4f805f9755e17e54318407b5fb20e4ffb7340213
Talha siddiqui <talhasiddiqui8888@gmail.com>
2016-02-28 20:56:37 -0800

    word  of caution

---
+++ Untitled2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("Enter any number");
+    scanf("%d",&i);
+
+    if(i==5)
+    {
+        printf("You Entered 5\n");
+    }
+        else
+        {
+        printf("You Entered something other than 5\n ");
+        }
+        return 0;
+
+}
